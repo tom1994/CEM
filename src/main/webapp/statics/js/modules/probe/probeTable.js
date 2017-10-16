c399a30445086b28873b722b6e7594d4c43d1630
Miao <Miao@DESKTOP-FT6C3CV>
2017-10-16 15:42:05 +0800

    探针列表部分增加探针组列表，并对页面重新进行排版

--- src/main/webapp/statics/js/modules/probe/probeTable.js
+++ src/main/webapp/statics/js/modules/probe/probeTable.js
@@ -6,7 +6,7 @@ var idArray = new Array();
 var testGroupNames = new Array();
 
 var probedata_handle = new Vue({
-    el: '#handle',
+    el: '#probehandle',
     data: {},
     mounted(){         /*动态加载测试任务组数据*/
         $.ajax({
@@ -16,11 +16,11 @@ var probedata_handle = new Vue({
             dataType: "json",
             /* contentType:"application/json",  /!*必须要,不可少*!/*/
             success: function (result) {
-                for(var i=0;i<result.page.list.length;i++){
-                    testGroupNames[i] = {message: result.page.list[i]}
-                }
-                probeform_data.testgroup_names = testGroupNames;    /*注意,这个js执行放在probeform_data和search_data之前才行*/
-                search_data.testgroup_names = testGroupNames;
+                // for(var i=0;i<result.page.list.length;i++){
+                //     testGroupNames[i] = {message: result.page.list[i]}
+                // }
+                // probeform_data.testgroup_names = testGroupNames;    /*注意,这个js执行放在probeform_data和search_data之前才行*/
+                // search_data.testgroup_names = testGroupNames;
             }
         });
     },
@@ -37,7 +37,7 @@ var probedata_handle = new Vue({
             for (var i = 0; i < 11; i++) {
                 forms[i].value = ""
             }
-            probeform_data.modaltitle = "测试机管理录入";
+            probeform_data.modaltitle = "探针理录入";
             /*修改模态框标题*/
             $('#myModal').modal('show');
 
@@ -130,7 +130,7 @@ var probedata_handle = new Vue({
             }
         },
         probesearch: function () {   /*查询监听事件*/
-            var data = getFormJson($('#searchcolums'));
+            var data = getFormJson($('#probesearch'));
             /*得到查询条件*/
             /*获取表单元素的值*/
             console.log(data);
@@ -145,6 +145,128 @@ var probedata_handle = new Vue({
 
     }
 });
+
+var probegroup_handle = new Vue({
+    el: '#grouphandle',
+    data: {},
+    mounted(){         /*动态加载测试任务组数据*/
+        $.ajax({
+            type: "POST",   /*GET会乱码*/
+            url: "../../cem/probe/list",//Todo:改成测试任务组的list方法
+            cache: false,  //禁用缓存
+            dataType: "json",
+            /* contentType:"application/json",  /!*必须要,不可少*!/*/
+            success: function (result) {
+                // for(var i=0;i<result.page.list.length;i++){
+                //     testGroupNames[i] = {message: result.page.list[i]}
+                // }
+                // probeform_data.testgroup_names = testGroupNames;    /*注意,这个js执行放在probeform_data和search_data之前才行*/
+                // search_data.testgroup_names = testGroupNames;
+            }
+        });
+    },
+    methods: {
+        groupadd: function () {   /*监听录入触发事件*/
+            status = 0;
+            /*状态0,表示录入*/
+            var forms = $('#probeform_data .form-control');
+
+            $('#probeform_data input[type=text]').prop("readonly", false);
+            /*去除只读状态*/
+            $('#probeform_data select').prop("disabled", false);
+
+            for (var i = 0; i < 11; i++) {
+                forms[i].value = ""
+            }
+            probeform_data.modaltitle = "新建探针组";
+            /*修改模态框标题*/
+            $('#myModal').modal('show');
+
+        },
+        groupupdate: function () {     /*监听编辑触发事件*/
+            status = 1;
+            /*状态1表示编辑*/
+            var trs = $('#probe_table tbody').find('tr:has(:checked)');
+            /*find被选中的行*/
+            var forms = $('#probeform_data .form-control');
+            console.log(trs.length + "表单对象:" + forms.length);
+
+            $('#probeform_data input[type=text]').prop("readonly", false);
+            /*去除只读状态*/
+            $('#probeform_data select').prop("disabled", false);
+
+            if (trs.length == 0) {
+                toastr.warning('请选择编辑项目！');
+            } else if (trs.length == 1) {
+                var tds = trs.find("td");
+                for (var i = 0; i < 6; i++) {                /*tds.eq(0).text()取得td的值,注意tds[0].text()取不到*/
+                    console.log(tds.eq(i + 3).text());
+                    forms[i].value = tds.eq(i + 2).text()
+                }
+                forms[6].value = tds.eq(10).text();
+                /*修改测试任务组*/
+                console.log(tds.eq(10).text());
+                for (var j = 0; j < 4; j++) {                /*tds.eq(0).text()取得td的值,注意tds[0].text()取不到*/
+                    console.log(tds.eq(j + 15).text());
+                    forms[j + 7].value = tds.eq(j + 15).text()
+                }
+                /*tds.each(function(){
+                 var td = $(this);
+                 console.log(td.text());//这个就是td的文本
+                 });*/
+                probeform_data.modaltitle = "测试机管理编辑";
+                /*修改模态框标题*/
+                $('#myModal').modal('show');
+            } else {
+                toastr.warning('请选择一条记录再编辑！');
+            }
+        },
+        groupview: function () {     /*查看监听事件*/
+            var trs = $('#probe_table tbody').find('tr:has(:checked)');
+            /*find被选中的行*/
+            var forms = $('#probeform_data .form-control');
+            if (trs.length == 0) {
+                toastr.warning('请选择查看项目！');
+            } else if (trs.length == 1) {
+                var tds = trs.find("td");
+                for (var i = 0; i < 6; i++) {                /*tds.eq(0).text()取得td的值,注意tds[0].text()取不到*/
+                    console.log(tds.eq(i + 2).text());
+                    forms[i].value = tds.eq(i + 2).text()
+                }
+                forms[6].value = tds.eq(10).text();
+                /*修改测试任务组*/
+                console.log(tds.eq(10).text());
+                for (var j = 0; j < 4; j++) {                /*tds.eq(0).text()取得td的值,注意tds[0].text()取不到*/
+                    console.log(tds.eq(j + 15).text());
+                    forms[j + 7].value = tds.eq(j + 15).text()
+                }
+                $('#probeform_data input[type=text]').prop("readonly", true);//将input元素设置为readonly
+                $('#probeform_data select').prop("disabled", true);//将select元素设置为不可变
+
+                probeform_data.modaltitle = "查看";
+                /*修改模态框标题*/
+                $('#myModal').modal('show');
+            } else {
+                toastr.warning('请选择一条记录再查看！');
+            }
+        },
+        groupsearch: function () {   /*查询监听事件*/
+            var data = getFormJson($('#searchcolums'));
+            /*得到查询条件*/
+            /*获取表单元素的值*/
+            console.log(data);
+            grouptable.groupdata = data;
+            grouptable.redraw();
+            /*根据查询条件重绘*/
+        },
+        reset: function () {    /*重置*/
+            probetable.reset();
+
+        }
+
+    }
+});
+
 function delete_ajax() {
     var ids = JSON.stringify(idArray);
     /*对象数组字符串*/
@@ -308,7 +430,6 @@ var search_data = new Vue({
         countys:probeform_data.countys,
         city_mans:probeform_data.city_mans,
         testgroup_names:[]
-
     }
 });
 /*选中表格事件*/
@@ -346,7 +467,7 @@ $(document).ready(function () {
 
 });
 
-// 注册
+// 探针列表
 var probetable = new Vue({
         el: '#probedata_table',
     data: {
@@ -465,6 +586,115 @@ var probetable = new Vue({
                             //调用DataTables提供的callback方法，代表数据已封装完成并传回DataTables进行渲染
                             //此时的数据需确保正确无误，异常判断应在执行此回调前自行处理完毕
                             callback(returnData);
+                    }
+                });
+            }
+        });
+    }
+});
+
+
+// 探针组列表
+var grouptable = new Vue({
+    el: '#probegroup_table',
+    data: {
+        headers: [
+            {title: ''},
+            {title: '<div class="checkbox"> <label> <input type="checkbox" id="checkAll"></label> </div>'},
+            {title: '<div style="display:none">id</div>'},
+            {title: '<div style="width:142px">探针组名</div>'},
+            {title: '<div style="width:52px">操作</div>'}
+        ],
+        rows: [],
+        dtHandle: null,
+        groupdata: {}
+
+    },
+    methods: {
+        reset: function () {
+            let vm = this;
+            vm.groupdata = {};
+            /*清空groupdata*/
+            vm.dtHandle.clear();
+            console.log("重置");
+            vm.dtHandle.draw();
+            /*重置*/
+        },
+        currReset: function () {
+            let vm = this;
+            vm.dtHandle.clear();
+            console.log("当前页面重绘");
+            vm.dtHandle.draw(false);
+            /*当前页面重绘*/
+        },
+        redraw: function () {
+            let vm = this;
+            vm.dtHandle.clear();
+            console.log("页面重绘");
+            vm.dtHandle.draw();
+            /*重绘*/
+        }
+    },
+    mounted() {
+        let vm = this;
+        vm.dtHandle = $(this.$el).DataTable({
+            columns: vm.headers,
+            data: vm.rows,
+            searching: false,
+            paging: true,
+            serverSide: true,
+            info: false,
+            ordering: false, /*禁用排序功能*/
+            oLanguage: {
+                sLengthMenu: "每页 _MENU_ 行数据",
+                oPaginate: {
+                    sNext: '<i class="fa fa-chevron-right" ></i>', /*图标替换上一页,下一页*/
+                    sPrevious: '<i class="fa fa-chevron-left" ></i>'
+                }
+            },
+            sDom: 'Rfrtlip', /*显示在左下角*/
+            ajax: function (data, callback, settings) {
+                //封装请求参数
+                let param = {};
+                param.limit = data.length;//页面显示记录条数，在页面显示每页显示多少项的时候
+                param.start = data.start;//开始的记录序号
+                param.page = (data.start / data.length) + 1;//当前页码
+                param.groupdata = JSON.stringify(vm.groupdata);
+                /*用于查询probe数据*/
+                console.log(param);
+                //ajax请求数据
+                $.ajax({
+                    type: "POST", /*GET会乱码*/
+                    url: "../../cem/probegroup/list",
+                    cache: false,  //禁用缓存
+                    data: param,  //传入组装的参数
+                    dataType: "json",
+                    success: function (result) {
+                        console.log(result);
+
+                        //封装返回数据
+                        let returnData = {};
+                        returnData.draw = data.draw;//这里直接自行返回了draw计数器,应该由后台返回
+                        returnData.recordsTotal = result.page.totalCount;//返回数据全部记录
+                        returnData.recordsFiltered = result.page.totalCount;//后台不实现过滤功能，每次查询均视作全部结果
+                        // returnData.data = result.page.list;//返回的数据列表
+                        // 重新整理返回数据以匹配表格
+                        let rows = [];
+                        var i = param.start+1;
+                        result.page.list.forEach(function (item) {
+                            let row = [];
+                            row.push(i++);
+                            row.push('<div class="checkbox"> <label> <input type="checkbox" name="selectFlag"></label> </div>');
+                            row.push('<div class="probe_id">'+item.id+'</div>');
+                            row.push(item.name);
+                            row.push('<a class="fontcolor" onclick="delete_this(this)" id='+item.id+'>删除</a>');
+                            rows.push(row);
+                        });
+                        returnData.data = rows;
+                        console.log(returnData);
+                        //调用DataTables提供的callback方法，代表数据已封装完成并传回DataTables进行渲染
+                        //此时的数据需确保正确无误，异常判断应在执行此回调前自行处理完毕
+                        callback(returnData);
                     }
                 });
             }
