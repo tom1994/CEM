0964b9da80b862dbe985df371bdc0c6614accf02
chengzhitian <1660841509@qq.com>
2017-11-06 10:27:59 +0800

    任务创建模块修改，新增新建模板页面 2017年11月6日

--- src/main/webapp/statics/js/modules/probe/templateNew.js
+++ src/main/webapp/statics/js/modules/probe/templateNew.js
@@ -1,38 +1,75 @@
 $(document).ready(function () {
-    $('#networkconnection').on('click', function () {
+    $('#netconnection').on('click', function () {
         if (this.checked) {
-            $("input[class='networkconnection']").each(function() {
+            $("input[class='netconnection']").each(function() {
                 $(this).prop("checked", true);
             });
         } else {
-            $("input[class='networkconnection']").each(function() {
+            $("input[class='netconnection']").each(function() {
                 $(this).prop("checked", false);
             });
         }
     });
-/*
-    $('#networkconnection').click(function () {
-        var select=$(this).prop("checked");
-        var selectall=$(".networkconnection").prop("checked",this);
-    });*/
-    $('#netlayerquality').click(function () {
-        var select=$(this).prop("checked");
-        var selectall=$(".netlayerquality").prop("checked",this);
+
+    $('#netlayerquality').on('click', function () {
+        if (this.checked) {
+            $("input[class='netlayerquality']").each(function() {
+                $(this).prop("checked", true);
+            });
+        } else {
+            $("input[class='netlayerquality']").each(function() {
+                $(this).prop("checked", false);
+            });
+        }
     });
-    $('#webbrowse').click(function () {
-        var select=$(this).prop("checked");
-        var selectall=$(".webbrowse").prop("checked",this);
+
+    $('#webbrowse').on('click', function () {
+        if (this.checked) {
+            $("input[class='webbrowse']").each(function() {
+                $(this).prop("checked", true);
+            });
+        } else {
+            $("input[class='webbrowse']").each(function() {
+                $(this).prop("checked", false);
+            });
+        }
     });
-    $('#filedownload').click(function () {
-        var select=$(this).prop("checked");
-        var selectall=$(".filedownload").prop("checked",this);
+
+    $('#filedownload').on('click', function () {
+        if (this.checked) {
+            $("input[class='filedownload']").each(function() {
+                $(this).prop("checked", true);
+            });
+        } else {
+            $("input[class='filedownload']").each(function() {
+                $(this).prop("checked", false);
+            });
+        }
     });
-    $('#onlinevideo').click(function () {
-        var select=$(this).prop("checked");
-        var selectall=$(".onlinevideo").prop("checked",this);
+
+    $('#onlinevideo').on('click', function () {
+        if (this.checked) {
+            $("input[class='onlinevideo']").each(function() {
+                $(this).prop("checked", true);
+            });
+        } else {
+            $("input[class='onlinevideo']").each(function() {
+                $(this).prop("checked", false);
+            });
+        }
     });
-    $('#onlinegame').click(function () {
-        var select=$(this).prop("checked");
-        var selectall=$(".onlinegame").prop("checked",this);
+
+    $('#onlinegame').on('click', function () {
+        if (this.checked) {
+            $("input[class='onlinegame']").each(function() {
+                $(this).prop("checked", true);
+            });
+        } else {
+            $("input[class='onlinegame']").each(function() {
+                $(this).prop("checked", false);
+            });
+        }
     });
+
 });
+
