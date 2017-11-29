d44c4da8fc329202a10f66bd277cfe84c6a38ad3
Ferrnya <30645944+Ferrnya@users.noreply.github.com>
2017-11-29 15:57:00 +0800

    调度策略表

--- src/main/webapp/statics/js/modules/evaluation/qualityRank.js
+++ src/main/webapp/statics/js/modules/evaluation/qualityRank.js
@@ -95,3 +95,81 @@ var getService = function (serviceId) {
         }
     });
 }
+
+var search_service = new Vue({
+    el: '#search',
+    data: {
+        /*name: [],
+        scheduler: [],
+        remark: []*/
+    },
+    // 在 `methods` 对象中定义方法
+    methods: {
+        testagentListsearch: function () {
+            var searchJson = getFormJson($('#probesearch'));
+            if((searchJson.startDate)>(searchJson.terminalDate)){
+                console.log("时间选择有误，请重新选择！");
+                $('#nonavailable_time').modal('show');
+            }else{
+                var ava_start=searchJson.startDate.substr(0,10);
+                var ava_terminal=searchJson.terminalDate.substr(0,10);
+                var start_time=searchJson.startDate.substr(11,15);
+                var terminal_time=searchJson.startDate.substr(11,15);
+                console.log(start_time);
+                var schedulepolicy = JSON.stringify(searchJson);
+                console.log(schedulepolicy);
+
+            }
+
+
+
+
+        }
+    }
+});
+
+function getFormJson(form) {      /*将表单对象变为json对象*/
+    var o = {};
+    var a = $(form).serializeArray();
+    $.each(a, function () {
+        if (o[this.name] !== undefined) {
+            if (!o[this.name].push) {
+                o[this.name] = [o[this.name]];
+            }
+            o[this.name].push(this.value || '');
+        } else {
+            o[this.name] = this.value || '';
+        }
+    });
+    return o;
+}
+
+//格式化日期
+Date.prototype.Format = function (fmt) {
+    var o = {
+        "y+": this.getFullYear(),
+        "M+": this.getMonth() + 1,                 //月份
+        "d+": this.getDate(),                    //日
+        "h+": this.getHours(),                   //小时
+        "m+": this.getMinutes(),                 //分
+        "s+": this.getSeconds(),                 //秒
+        "q+": Math.floor((this.getMonth() + 3) / 3), //季度
+        "S+": this.getMilliseconds()             //毫秒
+    };
+    for (var k in o) {
+        if (new RegExp("(" + k + ")").test(fmt)){
+            if(k == "y+"){
+                fmt = fmt.replace(RegExp.$1, ("" + o[k]).substr(4 - RegExp.$1.length));
+            }
+            else if(k=="S+"){
+                var lens = RegExp.$1.length;
+                lens = lens==1?3:lens;
+                fmt = fmt.replace(RegExp.$1, ("00" + o[k]).substr(("" + o[k]).length - 1,lens));
+            }
+            else{
+                fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+            }
+        }
+    }
+    return fmt;
+}
