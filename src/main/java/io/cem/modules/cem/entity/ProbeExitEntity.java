b0823f07e11866e53274e3197a9b24e693418410
Ferrnya <30645944+Ferrnya@users.noreply.github.com>
2018-03-26 20:36:15 +0800

    告警模版和告警信息

---
+++ src/main/java/io/cem/modules/cem/entity/ProbeExitEntity.java
@@ -0,0 +1,98 @@
+package io.cem.modules.cem.entity;
+
+import java.io.Serializable;
+
+
+
+/**
+ * 端口-出口对照表
+ * 
+ * @author ${author}
+ * @email ${email}
+ * @date 2018-03-26 19:47:28
+ */
+public class ProbeExitEntity implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	//
+	private Integer id;
+	//
+	private Integer probeId;
+	//
+	private String port;
+	//
+	private String exit;
+	//
+	private Integer status;
+
+	private String probeName;
+
+	/**
+	 * 设置：
+	 */
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	/**
+	 * 获取：
+	 */
+	public Integer getId() {
+		return id;
+	}
+	/**
+	 * 设置：
+	 */
+	public void setProbeId(Integer probeId) {
+		this.probeId = probeId;
+	}
+	/**
+	 * 获取：
+	 */
+	public Integer getProbeId() {
+		return probeId;
+	}
+	/**
+	 * 设置：
+	 */
+	public void setPort(String port) {
+		this.port = port;
+	}
+	/**
+	 * 获取：
+	 */
+	public String getPort() {
+		return port;
+	}
+	/**
+	 * 设置：
+	 */
+	public void setExit(String exit) {
+		this.exit = exit;
+	}
+	/**
+	 * 获取：
+	 */
+	public String getExit() {
+		return exit;
+	}
+	/**
+	 * 设置：
+	 */
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+	/**
+	 * 获取：
+	 */
+	public Integer getStatus() {
+		return status;
+	}
+
+	public String getProbeName() {
+		return probeName;
+	}
+
+	public void setProbeName(String probeName) {
+		this.probeName = probeName;
+	}
+}
